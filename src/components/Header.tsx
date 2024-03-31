9b204197078fac4562b9695a5781204a78fad118
logesh-kumar <phoenix.logu@gmail.com>
2024-04-01 00:01:05 +0530

    display user list in home page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { AppBar, Toolbar, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
 
 const Header: React.FC = () => (
   <AppBar position="static">
     <Toolbar>
-      <Typography variant="h6">GitHub User Explorer</Typography>
+      <Link to="/" style={{ textDecoration: "none", color: "white" }}>
+        <Typography variant="h6">GitHub user explorer</Typography>
+      </Link>
     </Toolbar>
   </AppBar>
 );
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,29 +1,8 @@
 import React from "react";
-import { Button, Typography, Container, Box } from "@mui/material";
-import { useNavigate } from "react-router-dom";
+import UserListPageListView from "./UserListPageListView";
 
 const HomePage: React.FC = () => {
-  const navigate = useNavigate();
-
-  return (
-    <Container maxWidth="sm" sx={{ mt: 8, textAlign: "center" }}>
-      <Typography variant="h2" gutterBottom>
-        Welcome to GitHub User Explorer
-      </Typography>
-      <Typography variant="h5" paragraph>
-        Discover GitHub users and their detailed profiles.
-      </Typography>
-      <Box sx={{ mt: 4 }}>
-        <Button
-          variant="contained"
-          size="large"
-          onClick={() => navigate("/users/list-view")}
-        >
-          Explore Users
-        </Button>
-      </Box>
-    </Container>
-  );
+  return <UserListPageListView />;
 };
 
 export default HomePage;
